44fdfe83e71d8116b440d3cc59e4cd222a852e2a
Horace <49237174+MHieu128@users.noreply.github.com>
2023-10-17 01:39:10 +0700

    Update search style

--- app.vue
+++ app.vue
@@ -8,8 +8,8 @@
           <div class="text-center w-1/2">
             <h1 class="text-8xl font-serif mb-10 text-white">Find your cat</h1>
             <div class="font-serif w-[100%] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
-              <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-non" placeholder="Search by cat type...">
-              <button class="bg-sky-500 px-10 text-white">Search</button>
+              <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none" placeholder="Search by cat type...">
+              <button class="bg-cyan-500 px-10 text-white">Search</button>
             </div>
           </div>
         </div>
